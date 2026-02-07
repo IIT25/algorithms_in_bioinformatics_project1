30e67b0da3a467368b0a941be64e2ce6cfc371ab
JIUHWA <jiuhwa@inf.elte.hu>
2026-02-07 14:06:19 +0100

    Score matrix calculation draft

--- global_linear.py
+++ global_linear.py
@@ -11,7 +11,20 @@ def read_fasta(filename: str, num_sequences: int = 2):
 
 def global_linear(filename: str, backtracking: bool = False):
     sequences = read_fasta(filename)
-    print(sequences)
-    pass
+    #print(sequences)
+    n = len(sequences[0])
+    m = len(sequences[1])
+    score_matrix = [list(range(n+1))] + [[i+1] + [0] * n for i in range(m)]
+    #print(score_matrix)
+    #TODO: Get these from input
+    gap_cost = 5
+    alaphabet = ["a", "c", "g", "t"]
+    cost_matrix = [[0, 5, 2, 5], [5, 0, 5, 2], [2, 5, 0, 5], [5, 2, 5, 0]]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+                score_matrix[i][j] = min(score_matrix[i-1][j-1] + cost_matrix[alaphabet.index(sequences[0][j-1])][alaphabet.index(sequences[1][i-1])],
+                                         score_matrix[i-1][j] + gap_cost,
+                                         score_matrix[i][j-1] + gap_cost)
+    print(score_matrix)
 
 global_linear("test1.fasta")
