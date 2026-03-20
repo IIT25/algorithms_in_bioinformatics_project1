bd1808aeec433a105ba5869a8d766f349331bd40
juannico007 <juan24092002@gmail.com>
2026-03-20 15:39:20 +0100

    Added handler for unresolved characters for the last part of the project
    It is taking all the possibilities, if it doesn't take that much to compute the approximations, we will try with all and keep the best result

--- alignment_utils.py
+++ alignment_utils.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO
 from Bio.Seq import Seq           
 from Bio.SeqRecord import SeqRecord
+from itertools import product
 
 def read_fasta(filename: str, num_sequences: int = 2):
     sequences = {}
@@ -8,7 +9,7 @@ def read_fasta(filename: str, num_sequences: int = 2):
         for i, record in enumerate(SeqIO.parse(handle, "fasta")):
             if i < num_sequences:
                 
-                sequences[record.name] = record.seq
+                sequences[record.description] = record.seq
     return sequences
        
 def check_sequences_validity(sequences: list, alphabet: list):
@@ -61,3 +62,76 @@ def write_alignment_in_fasta(alignment: list, filename: str, sequence_names: lis
         records = [SeqRecord(Seq(alignment[i]), id=sequence_names[i], description="align") for i in range(len(alignment))]
     with open(filename, "w") as f:
         SeqIO.write(records, f, "fasta")
+
+#Handlers for unknown characters in the sequences
+AMBIGUOUS_MAP = {
+    "N": ["A", "C", "G", "T"],
+    "R": ["A", "G"],
+    "S": ["C", "G"]
+}
+
+def expand_sequence(sequence: str, alphabet: list, ambiguous_map: dict):
+    """
+    Function to generate all possible sequences from unresolved characters
+    (This is doable under this problem because we only have around 8 unknown characters
+    But otherwise it would increase untractabilly)
+
+    Recieves:
+    - sequence: Sequence to expand
+    - alphabet: List of elements in the considered alphabet
+    - ambiguous_map: Dictionary defining how to go about the non resolved characters
+    """
+    alphabet_set = set(alphabet)
+    
+    # Build list of options per position
+    options_per_position = []
+    
+    for char in sequence:
+        if char in alphabet_set:
+            options_per_position.append([char])
+        elif char in ambiguous_map:
+            options_per_position.append(ambiguous_map[char])
+        else:
+            raise Exception(f"Invalid character found: {char}")
+    
+    # Generate all combinations
+    all_combinations = list(product(*options_per_position))
+    
+    # Convert tuples to strings
+    expanded_sequences = ["".join(comb) for comb in all_combinations]
+    
+    return expanded_sequences
+
+def process_sequences(sequences, alphabet, filename_base):
+    """
+    Function that processes all the sequences in a fasta file and creates a new 
+    sequence per each unresolved character. 
+    (This is doable under this problem because we only have around 8 unknown characters
+    But otherwise it would increase untractabilly)
+
+    Recieves:
+    - sequences: Dictionary of sequences
+    - alphabet: List of elements in the considered alphabet
+    - filename_base: String with the filename used to save the sequences
+    - ambiguous_map: Dictionary defining how to go about the non resolved characters
+
+    """
+    all_expanded = []
+    sequence_names = list(sequences.keys())
+    print(sequence_names)
+    sequences = sequences.values()
+
+    # Expand each sequence
+    for seq in sequences:
+        expanded = expand_sequence(seq, alphabet, AMBIGUOUS_MAP)
+        all_expanded.append(expanded)
+    
+    # Generate all combinations across sequences
+    from itertools import product
+    combined_alignments = list(product(*all_expanded))
+    
+    # Write each combination to a file
+    for idx, alignment in enumerate(combined_alignments):
+        filename = f"{filename_base}_{idx+1}.fasta"
+        
+        write_alignment_in_fasta(list(alignment), filename, sequence_names)
